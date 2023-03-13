85cafe864526f1b08585d6c8edabe9e4fc20c86e
Ermias Solomon <ermiyasst@gmail.com>
2023-03-13 14:41:23 +0300

    bug fix

--- IMS_backend/routes/employee.routes.js
+++ IMS_backend/routes/employee.routes.js
@@ -1,7 +1,22 @@
 const express = require("express");
-const { addEmployee, fetchAllEmployee, findEmployee, updateEmployee, deleteEmployee, countEmployee } = require("../controllers/employee.controller");
+const { addEmployee, fetchAllEmployee, findEmployee, updateEmployee, deleteEmployee, countEmployee, uploadLiabilityForm, downloadSignedLiabilityDoc } = require("../controllers/employee.controller");
 const router = express.Router();
+const path = require('path');
+const multer = require('multer');
 
+const PATH = path.resolve(__dirname, '../data/liability_doc');
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, PATH);
+  },
+  filename: (req, file, cb) => {
+    console.log("request - ", );
+    cb(null, 'emp-' + req.params.id + '-liability.pdf')
+  }
+});
+let upload = multer({
+  storage: storage
+});
 
 router.get("/countEmployee", countEmployee);
 router.post("/addEmployee", addEmployee);
@@ -9,5 +24,7 @@ router.get("/fetchAllEmployee", fetchAllEmployee);
 router.get("/findEmployee/:id", findEmployee);
 router.put("/updateEmployee/:id", updateEmployee);
 router.post("/deleteEmployee/:id", deleteEmployee);
+router.post("/uploadLiabilityForm/:id", upload.single("file"), uploadLiabilityForm);
+router.get("/downloadLiabilityDoc/:id", downloadSignedLiabilityDoc);
 
 module.exports = router;
